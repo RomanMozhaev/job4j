a98b0897e74645f17cc9b67b7bfd53dad9084f7f
RomanMozhaev <roman.mozhaev@gmail.com>
2019-04-13 21:38:13 +0300

    1.3. JavaDoc. Code convention[#132361]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,13 @@
 package ru.job4j.calculate;
-
+/**
+ * This is the program Calculate that asks you to say Hello World!
+ * @author Roman Mozhaev
+ */
 public class Calculate {
+	/**
+	 * This is a main method. It prints Say Hello World
+	 */	
     public static void main (String[] args) {
-        System.out.println("Say Hallo World!");
+        System.out.println("Say Hello World!");
     }
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Roman Mozhaev roman.mozhaev@gmail.com
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
