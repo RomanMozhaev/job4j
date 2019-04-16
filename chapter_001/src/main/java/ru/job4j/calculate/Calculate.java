8b40204dc88732bdb381b5e26750de88b6ab5efb
RomanMozhaev <roman.mozhaev@gmail.com>
2019-04-16 19:29:16 +0300

    Files updates. No critical changes. Test

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -4,9 +4,9 @@ package ru.job4j.calculate;
  * @author Roman Mozhaev
  */
 public class Calculate {
-	/**
-	 * This is a main method. It prints Say Hello World
-	 */	
+    /**
+     * This is a main method. It prints Say Hello World
+     */
     public static void main(String[] args) {
         System.out.println("Say Hello World!");
     }
