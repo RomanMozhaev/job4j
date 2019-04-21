e5d635297a973a98bfafc81274b517d97ae3683c
RomanMozhaev <roman.mozhaev@gmail.com>
2019-04-21 19:50:15 +0300

    5.5. Рефакторинг кода.[#132326]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,9 +1,12 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * Paint builds a pyramid.
  * @author RomanM
- * @version 1.0, April 21, 2019
+ * @version 2.0, April 21, 2019
+ * refactoring April 21, 2019
  */
 
 public class Paint {
@@ -13,19 +16,11 @@ public class Paint {
      * @return - triangle is printed by toString.
      */
     public String rightTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        int wight = height;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != wight; column++) {
-                if (column <= row) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> column <= row
+        );
     }
     /**
      * this method builds the triangle with left side hypothenuse.
@@ -33,19 +28,11 @@ public class Paint {
      * @return - triangle is printed by toString.
      */
     public String leftTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        int wight = height;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != wight; column++) {
-                if (wight - row - 1 <= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> height - row - 1 <= column
+        );
     }
     /**
      * this method builds the pyramid.
@@ -53,11 +40,26 @@ public class Paint {
      * @return - pyramid is printed by toString.
      */
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                height * 2 - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+
+        );
+    }
+
+    /**
+     * This is a common method with a basic logic.
+     * @param height - the height of pyramid or triangle
+     * @param wight - wight of pyramid of triangle
+     * @param predicate - the predicate for building of geometric object
+     * @return - the object is built by toString
+     */
+    private String loopBy(int height, int wight, BiPredicate<Integer, Integer> predicate) {
         StringBuilder screen = new StringBuilder();
-        int wight = height * 2 - 1;
         for (int row = 0; row != height; row++) {
             for (int column = 0; column != wight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (predicate.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
@@ -66,5 +68,6 @@ public class Paint {
             screen.append(System.lineSeparator());
         }
         return screen.toString();
+
     }
 }
