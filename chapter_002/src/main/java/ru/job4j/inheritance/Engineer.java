c8904f6a0c99cbf8de8638e03cc68771db049545
RomanMozhaev <roman.mozhaev@gmail.com>
2019-05-05 19:54:57 +0300

    1. Реализация профессий в коде[#132435]

---
+++ chapter_002/src/main/java/ru/job4j/inheritance/Engineer.java
@@ -0,0 +1,28 @@
+package ru.job4j.inheritance;
+
+public class Engineer extends Profession {
+    private String engineeringField;
+    private int doneProjects;
+
+    private Engineer(String name, String professionalArea, String jobPlace){
+        super(name, professionalArea, jobPlace);
+    }
+    public Engineer(String name, String professionalArea, String jobPlace, String engineeringField, int doneProjects) {
+        this(name, professionalArea, jobPlace);
+        this.engineeringField = engineeringField;
+        this.doneProjects = doneProjects;
+    }
+
+    public String getEngineeringField() {
+        return engineeringField;
+    }
+
+    public int getDoneProjects() {
+        return doneProjects;
+    }
+
+    public SpareParts repair(Equipment equipment) {
+        SpareParts parts = new SpareParts();
+        return parts;
+    }
+}
