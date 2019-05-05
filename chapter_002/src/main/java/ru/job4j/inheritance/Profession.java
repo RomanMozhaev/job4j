9187ad32ec34ccfd02a09efc27851f4718ec1788
RomanMozhaev <roman.mozhaev@gmail.com>
2019-05-05 19:54:57 +0300

    1. Реализация профессий в коде[#132435]

---
+++ chapter_002/src/main/java/ru/job4j/inheritance/Profession.java
@@ -0,0 +1,25 @@
+package ru.job4j.inheritance;
+
+public class Profession {
+    private String name;
+    private String professionalArea;
+    private String jobPlace;
+
+    public Profession (String name, String professionalArea, String jobPlace){
+        this.name = name;
+        this.professionalArea = professionalArea;
+        this.jobPlace = jobPlace;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProfessionalArea() {
+        return professionalArea;
+    }
+
+    public String getJobPlace() {
+        return jobPlace;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/inheritance/Teacher.java
@@ -0,0 +1,21 @@
+package ru.job4j.inheritance;
+
+public class Teacher extends Profession {
+    private String subject;
+
+    private Teacher(String name, String professionalArea, String jobPlace){
+        super(name, professionalArea, jobPlace);
+    }
+    public Teacher(String name, String professionalArea, String jobPlace, String subject) {
+        this(name, professionalArea, jobPlace);
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+    public char mark(Pupil pupil) {
+        char markPpl = 0;
+        return markPpl;
+    }
+}
