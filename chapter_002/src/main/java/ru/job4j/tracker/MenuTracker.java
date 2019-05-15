acc0d9e697dbcb54a22ac06947c977184857322c
RomanMozhaev <roman.mozhaev@gmail.com>
2019-05-15 20:08:03 +0300

    1. Создать действия UserAction[#132441]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
@@ -0,0 +1,282 @@
+package ru.job4j.tracker;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * the outside class for menu creating and inside classes for different actions.
+ */
+public class MenuTracker {
+
+    /**
+     * the constants for choosing of the menu options.
+     */
+    private static final String ADD = "0";
+    private static final String REPLACE = "1";
+    private static final String DELETE = "2";
+    private static final String FIND_ALL = "3";
+    private static final String FIND_BY_NAME = "4";
+    private static final String FIND_BY_ID = "5";
+    private static final String EXIT = "6";
+
+    /**
+     * the input provides user answers
+     */
+    private Input input;
+    /**
+     * the tracker contains tickets
+     */
+    private Tracker tracker;
+    /**
+     * the list of available actions
+     */
+    private List<UserAction> actions = new ArrayList<>();
+
+    /**
+     * the constructor assigns input and tracker fields.
+     * @param input
+     * @param tracker
+     */
+    MenuTracker(Input input, Tracker tracker) {
+        this.input = input;
+        this.tracker = tracker;
+    }
+
+//    public int getActionsLength() {
+//        return this.actions.size();
+//    }
+
+    /**
+     * the method adds all action-classes to the list.
+     */
+    public void fillActions() {
+        this.actions.add(new AddItem());
+        this.actions.add(new ReplaceItem());
+        this.actions.add(new DeleteItem());
+        this.actions.add(new FindAll());
+        this.actions.add(new FindItemsByName());
+        this.actions.add(new FindItemsById());
+        this.actions.add(new Exit());
+    }
+
+    /**
+     * the method calls the action according to the key of the command
+     * @param key - the key of the command
+     */
+    public void select(String key) {
+        int i = Integer.valueOf(key);
+        this.actions.get(i).execute(this.input, this.tracker);
+
+    }
+
+    /**
+     * the method prints the menu of the actions.
+     */
+    public void show() {
+        System.out.println("Menu:");
+        for (UserAction action : actions) {
+            System.out.println(action.info());
+        }
+    }
+    /**
+     * This class adds a new ticket to the array.
+     */
+    public class AddItem implements UserAction {
+        @Override
+        public String key() {
+            return ADD;
+        }
+        @Override
+        public void execute(Input input, Tracker tracker) {
+            System.out.println("------------ Adding of a new ticket --------------");
+            String name = input.ask("Enter the ticket name: ");
+            String desc = input.ask("Enter the ticket description: ");
+            long created = System.currentTimeMillis();
+            Item item = new Item(name, desc, created);
+            tracker.add(item);
+            System.out.println("------------ The New Ticket has ID: " + item.getId() + "-----------");
+        }
+        @Override
+        public  String info() {
+            return "0. Add New Ticket";
+        }
+    }
+    /**
+     * this class replaces the ticket.
+     */
+    public class ReplaceItem implements UserAction {
+        @Override
+        public String key() {
+            return REPLACE;
+        }
+        @Override
+        public void execute(Input input, Tracker tracker) {
+            System.out.println("------------ Editing of the ticket --------------");
+            String id = input.ask("Enter the ID:");
+            Item item = tracker.findById(id);
+            if (item == null) {
+                System.out.println("The ticket was not found.");
+            } else {
+                String name = input.ask("Enter a new name: ");
+                String desc = input.ask("Enter a new description: ");
+                long created = System.currentTimeMillis();
+                item = new Item(name, desc, created);
+                item.setId(id);
+                if (tracker.replace(id, item)) {
+                    System.out.println("------------ The changes were entered successfully ------------");
+                } else {
+                    System.out.println("------------ the operation failure. Please try again ------------");
+                }
+            }
+        }
+        @Override
+        public  String info() {
+            return "1. Edit The Ticket";
+        }
+    }
+    /**
+     * the class deletes the ticket with a corresponding ID.
+     */
+    public class DeleteItem implements UserAction {
+        @Override
+        public String key() {
+            return DELETE;
+        }
+        @Override
+        public void execute(Input input, Tracker tracker) {
+            System.out.println("------------ Deleting of the ticket --------------");
+            String id = input.ask("Enter the ID:");
+            if (tracker.delete(id)) {
+                System.out.println("The delete operation was completed successfully");
+            } else {
+                System.out.println("the operation failure. The ticket with the entered ID doesn't exist.");
+            }
+        }
+        @Override
+        public  String info() {
+            return "2. Delete The Ticket";
+        }
+    }
+    /**
+     * the method prints information from all entered tickets.
+     */
+    public class FindAll implements UserAction {
+        @Override
+        public String key() {
+            return FIND_ALL;
+        }
+        @Override
+        public void execute(Input input, Tracker tracker) {
+            System.out.println("------------ All tickets review --------------");
+            Item[] items = tracker.findAll();
+            TicketPrinting print = new TicketPrinting(items);
+            print.print();
+        }
+        @Override
+        public  String info() {
+            return "3. Review All Tickets";
+        }
+    }
+
+    /**
+     * this class finds all tickets with specific name
+     */
+    public class FindItemsByName implements UserAction {
+        @Override
+        public String key() {
+            return FIND_BY_NAME;
+        }
+        @Override
+        public void execute(Input input, Tracker tracker) {
+            System.out.println("------------ Finding all tickets with a specific name --------------");
+            String specName = input.ask("Enter the name for searching: ");
+            Item[] items = tracker.findByName(specName);
+            System.out.println("------------ The tickets with the specific name review --------------");
+            TicketPrinting print = new TicketPrinting(items);
+            print.print();
+        }
+        @Override
+        public  String info() {
+            return "4. Find  Tickets by Name";
+        }
+    }
+
+    /**
+     * the class finds a ticket with specific ID
+     */
+    public class FindItemsById implements UserAction {
+        @Override
+        public String key() {
+            return FIND_BY_ID;
+        }
+        @Override
+        public void execute(Input input, Tracker tracker) {
+            System.out.println("------------ Finding the ticket by ID --------------");
+            String id = input.ask("Enter the ID for searching: ");
+            Item item = tracker.findById(id);
+            if (item != null) {
+                Item[] items = new Item[1];
+                items[0] = item;
+                TicketPrinting print = new TicketPrinting(items);
+                print.print();
+            } else {
+                System.out.println("No ticket found.");
+            }
+        }
+        @Override
+        public  String info() {
+            return "5. Find A Ticket by ID";
+        }
+    }
+
+    /**
+     * the class for closing the application.
+     */
+    public class Exit implements UserAction {
+        @Override
+        public String key() {
+            return EXIT;
+        }
+        @Override
+        public void execute(Input input, Tracker tracker) {
+        }
+        @Override
+        public  String info() {
+            return "6. Exit";
+        }
+    }
+    /**
+     * this class prints the array of tickets
+     */
+    private class TicketPrinting {
+
+        Item[] items;
+
+        TicketPrinting(Item[] items) {
+            this.items = items;
+        }
+
+        private void print() {
+            if (items.length == 0) {
+                System.out.println("No ticket found.");
+            } else {
+                for (int i = 0; i < items.length; i++) {
+                    System.out.println("Order number: " + (i + 1));
+                    System.out.println("Ticket's ID: " + items[i].getId());
+                    System.out.println("Ticket's name: " + items[i].getName());
+                    System.out.println("Ticket's description: " + items[i].getDecs());
+                    DateFormat simple = new SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z");
+                    Date result = new Date(items[i].getTime());
+                    System.out.println("Ticket's date creation: " + simple.format(result));
+                    System.out.println("----------------------");
+                }
+            }
+        }
+    }
+
+}
+
+
--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,21 +1,10 @@
 package ru.job4j.tracker;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 /**
  * This class creates the user interface for working with ticket's system.
  */
 public class StartUI {
 
-    private static final String ADD = "0";
-    private static final String REPLACE = "1";
-    private static final String DELETE = "2";
-    private static final String FIND_ALL = "3";
-    private static final String FIND_BY_NAME = "4";
-    private static final String FIND_BY_ID = "5";
-    private static final String EXIT = "6";
     /**
      * receiving of user input
      */
@@ -40,154 +29,31 @@ public class StartUI {
      * the main program cycle.
      */
     public void init() {
+        MenuTracker menu = new MenuTracker(this.input, this.tracker);
+        menu.fillActions();
         boolean exit = false;
         while (!exit) {
-            this.showMenu();
+            menu.show();
             String answer = this.input.ask("Choose one option: ");
             switch (answer) {
-                case ADD:
-                    addItem();
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                    menu.select(answer);
                     break;
-                case REPLACE:
-                    replaceItem();
-                    break;
-                case DELETE:
-                    deleteItem();
-                    break;
-                case FIND_ALL:
-                    findAllItems();
-                    break;
-                case FIND_BY_NAME:
-                    findItemsByName();
-                    break;
-                case FIND_BY_ID:
-                    findItemById();
-                    break;
-                case EXIT:
+                case "6":
                     exit = true;
                     break;
+
                 default:
-                    System.out.println("Please try again.");
+                    System.out.println("Wrong selection. Try again.");
+                    break;
             }
         }
     }
-
-    /**
-     * This method adds a new ticket to the array.
-     */
-    private void addItem() {
-        System.out.println("------------ Adding of a new ticket --------------");
-        String name = this.input.ask("Enter the ticket name: ");
-        String desc = this.input.ask("Enter the ticket description: ");
-        long created = System.currentTimeMillis();
-        Item item = new Item(name, desc, created);
-        this.tracker.add(item);
-        System.out.println("------------ The New Ticket has ID: " + item.getId() + "-----------");
-    }
-
-    /**
-     * this method replaces the ticket.
-     */
-    private void replaceItem() {
-        System.out.println("------------ Editing of the ticket --------------");
-        String id = this.input.ask("Enter the ID:");
-        Item item = this.tracker.findById(id);
-        if (item == null) {
-            System.out.println("The ticket was not found.");
-        } else {
-            String name = this.input.ask("Enter a new name: ");
-            String desc = this.input.ask("Enter a new description: ");
-            long created = System.currentTimeMillis();
-            item = new Item(name, desc, created);
-            item.setId(id);
-            if (this.tracker.replace(id, item)) {
-                System.out.println("------------ The changes were entered successfully ------------");
-            } else {
-                System.out.println("------------ the operation failure. Please try again ------------");
-            }
-        }
-    }
-
-    /**
-     * the method deletes the ticket with a corresponding ID.
-     */
-    private void deleteItem() {
-        System.out.println("------------ Deleting of the ticket --------------");
-        String id = this.input.ask("Enter the ID:");
-        if (this.tracker.delete(id)) {
-            System.out.println("The delete operation was completed successfully");
-        } else {
-            System.out.println("the operation failure. The ticket with the entered ID doesn't exist.");
-        }
-    }
-    /**
-     * the method prints information from all entered tickets.
-     */
-    private void findAllItems() {
-        System.out.println("------------ All tickets review --------------");
-        Item[] items = this.tracker.findAll();
-        ticketPrinting(items);
-    }
-    /**
-     *
-     */
-    private void findItemsByName() {
-        System.out.println("------------ Finding all tickets with a specific name --------------");
-        String specName = this.input.ask("Enter the name for searching: ");
-        Item[] items = this.tracker.findByName(specName);
-        System.out.println("------------ The tickets with the specific name review --------------");
-        ticketPrinting(items);
-    }
-
-    /**
-     * this method prints the array of tickets
-     * @param items - the array of tickets for printing
-     */
-    private void ticketPrinting(Item[] items) {
-        if (items.length == 0) {
-            System.out.println("No ticket found.");
-        } else {
-            for (int i = 0; i < items.length; i++) {
-                System.out.println("Order number: " + (i + 1));
-                System.out.println("Ticket's ID: " + items[i].getId());
-                System.out.println("Ticket's name: " + items[i].getName());
-                System.out.println("Ticket's description: " + items[i].getDecs());
-                DateFormat simple = new SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z");
-                Date result = new Date(items[i].getTime());
-                System.out.println("Ticket's date creation: " + simple.format(result));
-                System.out.println("----------------------");
-            }
-        }
-    }
-
-    /**
-     * the method searches the ticket by ID
-     */
-    private void findItemById() {
-        System.out.println("------------ Finding the ticket by ID --------------");
-        String id = this.input.ask("Enter the ID for searching: ");
-        Item item = this.tracker.findById(id);
-        if (item != null) {
-            Item[] items = new Item[1];
-            items[0] = item;
-            ticketPrinting(items);
-        } else {
-            System.out.println("No ticket found.");
-        }
-
-    }
-
-    private void showMenu() {
-        System.out.println("Menu:");
-        System.out.println("0. Add New Ticket");
-        System.out.println("1. Edit The Ticket");
-        System.out.println("2. Delete The Ticket");
-        System.out.println("3. Review All Tickets");
-        System.out.println("4. Find  Tickets by Name");
-        System.out.println("5. Find A Ticket by ID");
-        System.out.println("6. Exit");
-    }
-
     /**
      * The program start.
      * @param args
