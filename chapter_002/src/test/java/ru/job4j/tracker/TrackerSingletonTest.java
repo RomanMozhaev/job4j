388be480c078eab45a314b78147c7f9eb86bfe01
RomanMozhaev <roman.mozhaev@gmail.com>
2019-05-23 19:38:14 +0300

    1. Singleton.[#132426]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerSingletonTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.tracker;
+
+        import org.junit.Before;
+        import org.junit.Test;
+
+        import static org.hamcrest.Matchers.is;
+        import static org.junit.Assert.assertThat;
+
+/**
+ * the tests for Tracker's methods.
+ * @author RomanM
+ * @version 1.1 May 9, 2019
+ */
+public class TrackerSingletonTest {
+
+    @Test
+    public void whenTrackerIsSingletonStaticField() {
+        TrackerSingleton1 trackerFirst = TrackerSingleton1.getInstance();
+        TrackerSingleton1 trackerSecond = TrackerSingleton1.getInstance();
+        assertThat(trackerFirst, is(trackerSecond));
+    }
+
+    @Test
+    public void whenTrackerIsSingletonStaticFinalField() {
+        TrackerSingleton2 trackerFirst = TrackerSingleton2.getInstance();
+        TrackerSingleton2 trackerSecond = TrackerSingleton2.getInstance();
+        assertThat(trackerFirst, is(trackerSecond));
+    }
+
+    @Test
+    public void whenTrackerIsSingletonStaticFinalClass() {
+        TrackerSingleton3 trackerFirst = TrackerSingleton3.getInstance();
+        TrackerSingleton3 trackerSecond = TrackerSingleton3.getInstance();
+        assertThat(trackerFirst, is(trackerSecond));
+    }
+
+    @Test
+    public void whenTrackerIsSingletonEnum() {
+        TrackerSingleton4 trackerFirst = TrackerSingleton4.INSTANCE;
+        TrackerSingleton4 trackerSecond = TrackerSingleton4.INSTANCE;
+        assertThat(trackerFirst, is(trackerSecond));
+    }
+
+
+
+}
