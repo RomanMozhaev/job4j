5523cba26ada49f5dbfa201e2955e072b9716316
RomanMozhaev <roman.mozhaev@gmail.com>
2019-06-07 21:04:28 +0300

    2. Сортировка User с использованием Comparator[#132380]

--- chapter_003/src/main/java/ru/job4j/sort/User.java
+++ chapter_003/src/main/java/ru/job4j/sort/User.java
@@ -1,5 +1,7 @@
 package ru.job4j.sort;
 
+import java.util.Objects;
+
 /**
  * the User contains fields Age and Name
  */
@@ -12,6 +14,14 @@ public class User implements Comparable<User> {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
     @Override
     public int compareTo(User o) {
         return this.age.compareTo(o.age);
@@ -20,5 +30,23 @@ public class User implements Comparable<User> {
     @Override
     public String toString() {
         return "User{" + "name='" + name + '\'' + ", age=" + age + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(name, user.name)
+                && Objects.equals(age, user.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
     }
 }
