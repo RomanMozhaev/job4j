c3abcb88418738df7de859521ff63af7549e57be
RomanMozhaev <roman.mozhaev@gmail.com>
2019-06-19 09:51:59 +0300

    4. Преобразование List в Map.[#132285]

--- chapter_004/src/main/java/ru/job4j/filter/School.java
+++ chapter_004/src/main/java/ru/job4j/filter/School.java
@@ -1,15 +1,31 @@
 package ru.job4j.filter;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+/**
+ * the class for working with students profiles
+ */
 public class School {
-
+    /**
+     * the method sorts profiles based on students' score
+     * @param students - list of students
+     * @param predict - selection criterion
+     * @return - students' list.
+     */
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> list = students.stream().filter(
+        return students.stream().filter(
                 student -> predict.test(student)
         ).collect(Collectors.toList());
-        return list;
     }
+
+    public Map<String, Student> studentMapCreate(List<Student> students) {
+        return students.stream().distinct().collect(Collectors.toMap(
+                student -> student.getSurname(),
+                student -> student
+        ));
+    }
+
 }
--- chapter_004/src/test/java/ru/job4j/filter/SchoolTest.java
+++ chapter_004/src/test/java/ru/job4j/filter/SchoolTest.java
@@ -3,6 +3,7 @@ package ru.job4j.filter;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,28 +13,33 @@ public class SchoolTest {
      * List of students
      */
     private List<Student> students = List.of(
-                new Student(70),
-                new Student(65),
-                new Student(95),
-                new Student(10),
-                new Student(20),
-                new Student(90),
-                new Student(40),
-                new Student(60)
+                new Student(70, "Petrov"),
+                new Student(65, "Ivanov"),
+                new Student(95, "Petrov"),
+                new Student(10, "Sidorov"),
+                new Student(20, "Kotov"),
+                new Student(90, "Nikitin"),
+                new Student(40, "Laptev"),
+                new Student(60, "Sokolov")
             );
     private List<Student> class10A = List.of(
-                new Student(70),
-                new Student(95),
-                new Student(90)
+                new Student(70, "Petrov"),
+                new Student(95, "Petrov"),
+                new Student(90, "Nikitin")
             );
     private List<Student> class10B = List.of(
-                new Student(65),
-                new Student(60)
+                new Student(65, "Ivanov"),
+                new Student(60, "Sokolov")
             );
     private List<Student> class10C = List.of(
-                new Student(10),
-                new Student(20),
-                new Student(40)
+                new Student(10, "Sidorov"),
+                new Student(20, "Kotov"),
+                new Student(40, "Laptev")
+            );
+    private Map<String, Student> class10cMap = Map.of(
+            "Sidorov", new Student(10, "Sidorov"),
+            "Kotov", new Student(20, "Kotov"),
+            "Laptev", new Student(40, "Laptev")
             );
     /**
      * Test for 10A
@@ -42,7 +48,7 @@ public class SchoolTest {
     public void when70MoreThen10A() {
         School school = new School();
         List<Student> result = school.collect(this.students, student -> (student.getScore() >= 70 && student.getScore() <= 100));
-        assertThat(result.toString(), is(class10A.toString()));
+        assertThat(result, is(this.class10A));
     }
     /**
      * Test for 10B
@@ -51,7 +57,7 @@ public class SchoolTest {
     public void when50to70Then10B() {
         School school = new School();
         List<Student> result = school.collect(this.students, student -> (student.getScore() < 70 && student.getScore() >= 50));
-        assertThat(result.toString(), is(class10B.toString()));
+        assertThat(result, is(this.class10B));
     }
     /**
      * Test for 10C
@@ -60,6 +66,12 @@ public class SchoolTest {
     public void when50lessThen10C() {
         School school = new School();
         List<Student> result = school.collect(this.students, student -> (student.getScore() < 50));
-        assertThat(result.toString(), is(class10C.toString()));
+        assertThat(result, is(this.class10C));
+    }
+    @Test
+    public void whenMapCreated() {
+        School school = new School();
+        Map<String, Student> result = school.studentMapCreate(this.class10C);
+        assertThat(result, is(class10cMap));
     }
 }
