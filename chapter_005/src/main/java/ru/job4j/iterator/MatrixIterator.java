d65dcfbd2fb6628946d2cd8a09cdeb86f7cc5af2
RomanMozhaev <roman.mozhaev@gmail.com>
2019-06-28 08:54:40 +0300

    5.1.1. Итератор для двухмерного массива int[][][#154427]

--- chapter_005/src/main/java/ru/job4j/iterator/MatrixIterator.java
+++ chapter_005/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -2,7 +2,31 @@ package ru.job4j.iterator;
 
 import java.util.Iterator;
 
-public class MatrixIterator  {
+public class MatrixIterator implements Iterator<Integer> {
 
+    private final int [][] matrix;
+    int column;
+    int row;
 
+    public MatrixIterator(final int[][] matrix) {
+        this.matrix = matrix;
+        this.column = 0;
+        this.row = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return row < matrix.length;
+    }
+
+    @Override
+    public Integer next() {
+        int result = this.matrix[this.row][this.column];
+        this.column++;
+        if (this.column == this.matrix[this.row].length) {
+            this.row++;
+            this.column = 0;
+        }
+        return result;
+    }
 }
--- chapter_005/src/test/java/ru.job4j.iterator/MatrixIteratorTest.java
+++ chapter_005/src/test/java/ru.job4j.iterator/MatrixIteratorTest.java
@@ -2,8 +2,26 @@ package ru.job4j.iterator;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class MatrixIteratorTest {
+
+    @Test
+    public void whenTest() {
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8, 9, 10},
+            {11, 12},
+            {13}
+        };
+        int[] expect = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        int[] result = new int[13];
+        MatrixIterator it = new MatrixIterator(matrix);
+        for (int i = 0; i < 13; i++) {
+            result[i] = it.next();
+        }
+        assertThat(result, is(expect));
+    }
 
 }
