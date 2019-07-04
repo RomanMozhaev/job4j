a56d57fdbb0eec48ff6f38491188ece3c595f384
RomanMozhaev <roman.mozhaev@gmail.com>
2019-07-04 21:15:38 +0300

    5.3.2. Создать контейнер на базе связанного списка [#154405]

--- chapter_005/src/test/java/ru/job4j/dynamiclinkedlist/DynamicLinkedListTest.java
+++ chapter_005/src/test/java/ru/job4j/dynamiclinkedlist/DynamicLinkedListTest.java
@@ -37,5 +37,45 @@ public class DynamicLinkedListTest {
         }
         assertThat(sum, is(21));
     }
+    @Test
+    public void whenDeletedThenNext() {
+        this.list.remove(4);
+        assertThat(this.list.get(4), is(6));
+    }
+    @Test
+    public void whenDeletedLastThenSizeFive() {
+        this.list.remove(5);
+        assertThat(this.list.getLength(), is(5));
+    }
+
+    @Test
+    public void whenDeletedFirstThenSecondHasIndexZero() {
+        this.list.remove(0);
+        assertThat(this.list.get(0), is(2));
+    }
+
+    @Test
+    public void whenDeletedAllInsteadOneThenSix() {
+        this.list.remove(0);
+        this.list.remove(0);
+        this.list.remove(0);
+        this.list.remove(0);
+        this.list.remove(0);
+        this.list.remove(0);
+
+        assertThat(this.list.getLength(), is(0));
+    }
+    @Test
+    public void whenDeletedAllInsteadOneThenFive() {
+        this.list.remove(0);
+        this.list.remove(0);
+        this.list.remove(0);
+        this.list.remove(0);
+        this.list.remove(1);
+
+
+        assertThat(this.list.get(0), is(5));
+    }
+
 
 }
