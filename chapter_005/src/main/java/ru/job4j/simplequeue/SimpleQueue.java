8831e132d4b110bc8ac8eccbe0b3a6a3cae32b2d
RomanMozhaev <roman.mozhaev@gmail.com>
2019-07-07 19:54:44 +0300

    5.3.3.1 Очередь на двух стеках[#154406] rev2

--- chapter_005/src/main/java/ru/job4j/simplequeue/SimpleQueue.java
+++ chapter_005/src/main/java/ru/job4j/simplequeue/SimpleQueue.java
@@ -8,14 +8,34 @@ private SimpleStack<T> stack1 = new SimpleStack<>();
 private SimpleStack<T> stack2 = new SimpleStack<>();
 
     public T poll() {
-        while (!stack1.isEmpty()) {
-            stack2.push(stack1.poll());
-        }
-        T result = stack2.poll();
-        while (!stack2.isEmpty()) {
-            stack1.push(stack2.poll());
+        T result = null;
+        int phase = 0;
+        while (phase != 2) {
+            if (phase == 0) {
+                stack2.push(stack1.poll());
+                if (stack1.isEmpty()) {
+                    result = stack2.poll();
+                    phase = 1;
+                }
+            }
+            if (phase == 1) {
+                stack1.push(stack2.poll());
+                if (stack2.isEmpty()) {
+                    phase = 2;
+                }
+            }
         }
         return result;
+
+
+//        while (!stack1.isEmpty()) {
+//            stack2.push(stack1.poll());
+//        }
+//        T result = stack2.poll();
+//        while (!stack2.isEmpty()) {
+//            stack1.push(stack2.poll());
+//        }
+//        return result;
     }
 
     public void push(T value) {
