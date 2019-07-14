b5d272f803b80f1c84798900a4052c658c790765
RomanMozhaev <roman.mozhaev@gmail.com>
2019-07-14 21:09:16 +0300

    Статистику по коллекции.[#154398]

---
+++ chapter_005/src/main/java/ru/job4j/analize/Analise.java
@@ -0,0 +1,47 @@
+package ru.job4j.analize;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analise {
+
+
+    public Info diff(List<User> previous, List<User> current) {
+        Info result;
+        if (previous == null) {
+            if (current == null) {
+                result = new Info(0, 0, 0);
+            } else {
+                result = new Info(current.size(), 0, 0);
+            }
+        } else {
+            if (current == null) {
+                result = new Info(0, 0, previous.size());
+            } else {
+                int changed = 0;
+                int added = 0;
+                Map<Integer, User> map = previous.stream()
+                        .flatMap(Stream:: ofNullable)
+                        .collect(Collectors.toMap(
+                                e -> e.getId(),
+                                e -> e
+                        ));
+                for (User user : current) {
+                    if (map.containsKey(user.getId())) {
+                        if (!map.containsValue(user)) {
+                            changed++;
+                        }
+                    } else {
+                        added++;
+                    }
+                }
+                int deleted = previous.size() - current.size() + added;
+                result = new Info(added, changed, deleted);
+            }
+        }
+
+        return result;
+
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/analize/AnaliseTest.java
@@ -0,0 +1,92 @@
+package ru.job4j.analize;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class AnaliseTest {
+
+    @Test
+    public void whenAnaliseThenSomeChanges() {
+
+        List<User> previous = Arrays.asList(
+                new User(113, "First"),
+                new User(114, "Second"),
+                new User(115, "third"),
+                new User(116, "forth"),
+                new User(117, "fifth")
+                );
+        List<User> current = Arrays.asList(
+                new User(113, "First Modified"),
+                new User(115, "third"),
+                new User(116, "forth Modified"),
+                new User(117, "fifth"),
+                new User(118, "sixth")
+                );
+        Analise analise = new Analise();
+        Info info = analise.diff(previous, current);
+        Info expect = new Info(1, 2, 1);
+        assertThat(info, is(expect));
+
+    }
+    @Test
+    public void whenAnaliseThenAllDeleted() {
+
+        List<User> previous = Arrays.asList(
+                new User(113, "First"),
+                new User(114, "Second"),
+                new User(115, "third"),
+                new User(116, "forth"),
+                new User(117, "fifth")
+        );
+        List<User> current = new ArrayList<>();
+        Analise analise = new Analise();
+        Info info = analise.diff(previous, current);
+        Info expect = new Info(0, 0, 5);
+        assertThat(info, is(expect));
+    }
+    @Test
+    public void whenAnaliseThenAllAdded() {
+
+        List<User> current = Arrays.asList(
+                new User(113, "First"),
+                new User(114, "Second"),
+                new User(115, "third"),
+                new User(116, "forth"),
+                new User(117, "fifth")
+        );
+        List<User> previous = new ArrayList<>();
+        Analise analise = new Analise();
+        Info info = analise.diff(previous, current);
+        Info expect = new Info(5, 0, 0);
+        assertThat(info, is(expect));
+    }
+    @Test
+    public void whenAnaliseThenDuplicates() {
+
+        List<User> previous = Arrays.asList(
+                new User(113, "First"),
+                new User(114, "Second"),
+                new User(115, "third"),
+                new User(116, "forth"),
+                new User(117, "fifth")
+        );
+        List<User> current = Arrays.asList(
+                new User(113, "First Modified"),
+                new User(115, "third"),
+                new User(116, "forth Modified"),
+                new User(117, "fifth"),
+                new User(118, "sixth")
+        );
+        Analise analise = new Analise();
+        Info info = analise.diff(previous, current);
+        Info expect = new Info(1, 2, 1);
+        assertThat(info, is(expect));
+
+    }
+}
