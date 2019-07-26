8e9b4091449d9d7f2fd18d058f2e9b61e9f199ab
RomanMozhaev <roman.mozhaev@gmail.com>
2019-07-26 20:00:55 +0300

    2. Анализ доступности сервера.[#154443] rev 2

--- chapter_006/src/test/java/ru/job4j/io/AnalysisTest.java
+++ chapter_006/src/test/java/ru/job4j/io/AnalysisTest.java
@@ -15,8 +15,8 @@ public class AnalysisTest {
     @Test
     public void whenSourceThenTarget() {
         Analysis analysis = new Analysis();
-        String source = "/home/roman/Projects/job4j/chapter_006/src/main/java/ru/job4j/io/unavailable.csv";
-        String target = "/home/roman/Projects/job4j/chapter_006/src/main/java/ru/job4j/io/time.txt";
+        String source = System.getProperty("java.io.tmpdir") + "/unavailable.csv";
+        String target = System.getProperty("java.io.tmpdir") + "/time.txt";
         analysis.unavailable(source, target);
         Optional<String> string = Optional.empty();
         try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
@@ -32,8 +32,8 @@ public class AnalysisTest {
     @Test
     public void whenSource2ThenTarget2() {
         Analysis analysis = new Analysis();
-        String source = "/home/roman/Projects/job4j/chapter_006/src/main/java/ru/job4j/io/unavailable2.csv";
-        String target = "/home/roman/Projects/job4j/chapter_006/src/main/java/ru/job4j/io/time2.txt";
+        String source = System.getProperty("java.io.tmpdir") + "/unavailable2.csv";
+        String target = System.getProperty("java.io.tmpdir") + "/time2.txt";
         analysis.unavailable(source, target);
         Optional<String> string = Optional.empty();
         try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
