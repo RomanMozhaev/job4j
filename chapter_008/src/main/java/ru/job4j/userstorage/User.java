6a2777fa9962553df781a787a5d7a9d78b7d7404
RomanMozhaev <roman.mozhaev@gmail.com>
2019-12-03 21:56:15 +0000

    2. Класс хранилища пользователей UserStorage[#202375] rev2

--- chapter_008/src/main/java/ru/job4j/userstorage/User.java
+++ chapter_008/src/main/java/ru/job4j/userstorage/User.java
@@ -1,5 +1,7 @@
 package ru.job4j.userstorage;
 
+import java.util.Objects;
+
 public class User {
     private final int id;
     private int amount;
@@ -15,5 +17,23 @@ public class User {
 
     public int getAmount() {
         return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return id == user.id
+                && amount == user.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, amount);
     }
 }
