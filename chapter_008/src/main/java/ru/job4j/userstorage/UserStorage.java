84b861957724a4f50a0af3235b302d11e08ecef4
RomanMozhaev <roman.mozhaev@gmail.com>
2019-12-07 13:23:12 +0000

    2. Класс хранилища пользователей UserStorage[#202375] rev 19-12-7

--- chapter_008/src/main/java/ru/job4j/userstorage/UserStorage.java
+++ chapter_008/src/main/java/ru/job4j/userstorage/UserStorage.java
@@ -1,6 +1,8 @@
 package ru.job4j.userstorage;
 
 import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 /**
@@ -16,60 +18,56 @@ public class UserStorage {
     /**
      * the main constructor.
      */
-    public UserStorage(ConcurrentMap<Integer, User> map) {
-        this.userMap = map;
+    public UserStorage() {
+        this.userMap = new ConcurrentHashMap<>();
     }
 
     /**
      * the method for adding user into the map.
+     *
      * @param user - user for adding.
      * @return true if it was added, false - if not.
      */
-    public boolean add(User user) {
-        synchronized (this.userMap) {
-            return this.userMap.putIfAbsent(user.getId(), user) == null;
-        }
+    public synchronized boolean add(User user) {
+        return this.userMap.putIfAbsent(user.getId(), user) == null;
     }
 
     /**
      * the method for replacing data if the user with same id is mapped.
+     *
      * @param user - the user for replacing
      * @return true if the user id was mapped and the user was replaced, false if not.
      */
-    public boolean update(User user) {
-        synchronized (this.userMap) {
-            return this.userMap.replace(user.getId(), user) != null;
-        }
+    public synchronized boolean update(User user) {
+        return this.userMap.replace(user.getId(), user) != null;
     }
 
     /**
      * the method for deleting user from the map if key and amount are equal.
+     *
      * @param user the user which should be deleted.
      * @return true if the user was deleted or false if not.
      */
-    public boolean delete(User user) {
-        synchronized (this.userMap) {
-            return this.userMap.remove(user.getId(), user);
-        }
+    public synchronized boolean delete(User user) {
+        return this.userMap.remove(user.getId(), user);
     }
 
     /**
      * the method for transferring amount from one user to another.
+     *
      * @param fromId - the supplier user id
-     * @param toId - the consumer user id
+     * @param toId   - the consumer user id
      * @param amount the value for transferring
      * @return true if users are mapped and supplier's amount more or equal to amount.
      */
-    public boolean transfer(int fromId, int toId, int amount) {
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
         boolean result = false;
-        synchronized (this.userMap) {
-            User userFrom = this.userMap.get(fromId);
-            User userTo = this.userMap.get(toId);
-            if (userFrom != null && userTo != null && userFrom.getAmount() >= amount) {
-                update(new User(userFrom.getId(), userFrom.getAmount() - amount));
-                update(new User(userTo.getId(), userTo.getAmount() + amount));
-                result = true;
-            }
+        User userFrom = this.userMap.get(fromId);
+        User userTo = this.userMap.get(toId);
+        if (userFrom != null && userTo != null && userFrom.getAmount() >= amount) {
+            update(new User(userFrom.getId(), userFrom.getAmount() - amount));
+            update(new User(userTo.getId(), userTo.getAmount() + amount));
+            result = true;
         }
         return result;
     }
