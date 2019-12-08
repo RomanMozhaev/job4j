5a2cb17d14bd0abdfaeff6537ff483f8f551f54c
RomanMozhaev <roman.mozhaev@gmail.com>
2019-12-08 22:54:10 +0000

    1. Реализовать шаблон Producer Consumer.[#202363]

---
+++ chapter_008/src/main/java/ru/job4j/producerconsumer/ProducerConsumer.java
@@ -0,0 +1,27 @@
+package ru.job4j.producerconsumer;
+
+/**
+ * the class for starting the Producer-Consumer program.
+ */
+public class ProducerConsumer {
+
+    /**
+     * the main method starts threads and has the cycle which interrupts consumer's thread to let it know
+     * that producer's thread was terminated.
+     *
+     * @param args are not used.
+     */
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        Producer p = new Producer(queue);
+        Consumer c = new Consumer(queue);
+        p.t.start();
+        c.t.start();
+        do {
+            if (p.t.getState() == Thread.State.TERMINATED) {
+                c.t.interrupt();
+            }
+        } while (c.t.getState() != Thread.State.TERMINATED);
+
+    }
+}
