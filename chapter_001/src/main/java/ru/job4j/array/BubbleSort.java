57ef72a3c6ff6ac262481d49d0d125d607c291d5
RomanMozhaev <roman.mozhaev@gmail.com>
2019-12-11 14:31:40 +0000

    CheckStyle mistake correction

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -15,7 +15,7 @@ public class BubbleSort {
 				for (int i = 0; i < array.length - j; i++) {
 					if (array[i] > array[i + 1]) {
 						int temp = array[i];
-						array[i] = array [i + 1];
+						array[i] = array[i + 1];
 						array[i + 1] = temp;
 					}
 				}
