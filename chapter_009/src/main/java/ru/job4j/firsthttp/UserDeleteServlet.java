19c2cdb71e59657307c9cf0a951aa3ff0f72b2ad
RomanMozhaev <roman.mozhaev@gmail.com>
2020-01-02 22:23:46 +0000

    2. Создание DBStore[#202322]

---
+++ chapter_009/src/main/java/ru/job4j/firsthttp/UserDeleteServlet.java
@@ -0,0 +1,80 @@
+package ru.job4j.firsthttp;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * the servlet deletes the user from the memory.
+ */
+public class UserDeleteServlet extends HttpServlet {
+
+    /**
+     * the instance with working methods.
+     */
+    private final Validate validate = ValidateService.getInstance();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        if (action == null) {
+            createDeleteForm(req, resp);
+        } else {
+            delete(req, resp);
+        }
+    }
+
+    /**
+     * shows the user's data which will be deleted.
+     * @param req
+     * @param resp
+     * @throws IOException
+     */
+    private void createDeleteForm(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        String id = req.getParameter("id");
+        User user = this.validate.findById(Integer.parseInt(id));
+        if (user != null) {
+            req.setAttribute("user", user);
+            this.getServletContext().getRequestDispatcher("/delete.jsp").forward(req, resp);
+        } else {
+            req.setAttribute("message", "The user was not found.");
+            this.getServletContext().getRequestDispatcher("/result.jsp").forward(req, resp);
+        }
+    }
+
+    /**
+     * deletes the user.
+     * @param req
+     * @param resp
+     * @throws IOException
+     */
+    private void delete(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        String stgId = req.getParameter("id");
+        String message;
+        if (this.validate.delete(new User(intOrDef(stgId, -1)))) {
+            message = "The user was successfully deleted.";
+        } else {
+            message = "The user was not deleted.";
+        }
+        req.setAttribute("message", message);
+        this.getServletContext().getRequestDispatcher("/result.jsp").forward(req, resp);
+    }
+
+    /**
+     * the method converts string to int or return def
+     * @param srgId - the string for converting to int
+     * @param def - default int
+     * @return int
+     */
+    private int intOrDef(String srgId, int def) {
+        int id;
+        try {
+            id = Integer.parseInt(srgId);
+        } catch (NumberFormatException e) {
+            id = def;
+        }
+        return id;
+    }
+}
---
+++ chapter_009/src/main/webapp/create.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="<%=request.getContextPath()%>/create" method="post">
+    Name : <input type="text" name="name" placeholder="enter the name (required)" size=60>
+    <br>
+    Email : <input type="text" name="email" placeholder="enter the email" size=60>
+    <br>
+    <input type="submit" value="add the new user">
+</form>
+</body>
+</html>
