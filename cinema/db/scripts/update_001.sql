77ccd4cc94387871b89ca4ac85c8e2c3622d932d
RomanMozhaev <roman.mozhaev@gmail.com>
2020-02-07 21:17:50 +0000

    Сервис - Кинотеатр[#202353]

---
+++ cinema/db/scripts/update_001.sql
@@ -0,0 +1,2 @@
+CREATE TABLE halls (hall_id int, row int, place int, account_id int);
+CREATE TABLE accounts (account_id serial primary key, name varchar(200), phone int);
---
+++ cinema/db/scripts/update_002.sql
@@ -0,0 +1,9 @@
+INSERT INTO halls VALUES (1, 1, 1, -1);
+INSERT INTO halls VALUES (1, 1, 2, -1);
+INSERT INTO halls VALUES (1, 1, 3, -1);
+INSERT INTO halls VALUES (1, 2, 1, -1);
+INSERT INTO halls VALUES (1, 2, 2, -1);
+INSERT INTO halls VALUES (1, 2, 3, -1);
+INSERT INTO halls VALUES (1, 3, 1, -1);
+INSERT INTO halls VALUES (1, 3, 2, -1);
+INSERT INTO halls VALUES (1, 3, 3, -1);
